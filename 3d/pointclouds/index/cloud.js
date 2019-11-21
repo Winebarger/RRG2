8ef832c5c8f1912207be1e7049148b6d90e8b10a
Winebarger <54688185+Winebarger@users.noreply.github.com>
2019-11-21 16:59:30 -0500

    Add 3d potree

---
+++ 3d/pointclouds/index/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 3038590,
+    "boundingBox": {
+        "lx": 5518290.09,
+        "ly": 3835905.62,
+        "lz": 669.3499999999999,
+        "ux": 5520290.08,
+        "uy": 3837905.6100000005,
+        "uz": 2669.3400000002234
+    },
+    "tightBoundingBox": {
+        "lx": 5518290.09,
+        "ly": 3835905.62,
+        "lz": 669.3499999999999,
+        "ux": 5520290.08,
+        "uy": 3837905.61,
+        "uz": 1292.35
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 17.32042121887207,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
